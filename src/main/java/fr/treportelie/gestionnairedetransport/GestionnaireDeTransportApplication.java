36860255b022af4722fb0eef0b6be77cf1b1a023
Tykok <treportelie12@gmail.com>
2022-04-13 13:49:11 +0200

    Init spring project

---
+++ src/main/java/fr/treportelie/gestionnairedetransport/GestionnaireDeTransportApplication.java
@@ -0,0 +1,13 @@
+package fr.treportelie.gestionnairedetransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireDeTransportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionnaireDeTransportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/treportelie/gestionnairedetransport/GestionnaireDeTransportApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.treportelie.gestionnairedetransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireDeTransportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
